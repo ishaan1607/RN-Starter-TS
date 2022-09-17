ff288aa66c9c5574c5eba18c3305202bebc80553
Ishaan Jain <ishaan.jain@likeminds.community>
2022-09-17 20:08:05 +0530

    list component added

--- App.tsx
+++ App.tsx
@@ -3,15 +3,17 @@ import {createStackNavigator} from 'react-navigation-stack';
 import ComponentsScreen from './src/screens/ComponentsScreen';
 import HomeScreens from './src/screens/HomeScreens';
 import JsxExercise from './src/screens/JsxExercise';
+import ListScreen from './src/screens/ListScreen';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreens,
     Components:ComponentsScreen,
-    Exercise:JsxExercise
+    Exercise:JsxExercise,
+    List:ListScreen
   },
   {
-    initialRouteName: 'Exercise',
+    initialRouteName: 'List',
     defaultNavigationOptions: {
       title: 'App',
     },
