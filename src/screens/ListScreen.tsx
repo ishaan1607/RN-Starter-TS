7b2f2d97851d53bed4b05c9d47b37ce5700306c1
Ishaan Jain <ishaan.jain@likeminds.community>
2022-09-17 20:31:46 +0530

    flatlist created

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
 const ListScreen = () => {
+  const friends = [
+    {name: 'Ishaan'},
+    {name: 'Satvik'},
+    {name: 'Sargam'},
+    {name: 'Sidhi'},
+    {name: 'Vishant'},
+    {name: 'Maddy'},
+    {name: 'JJ'},
+    {name: 'Anjali'},
+    {name: 'Agrim'},
+    {name: 'Ayushi'},
+  ];
+
   return (
-    <View>
-      <Text>ListScreen</Text>
-    </View>
+    <FlatList
+      keyExtractor={friend => friend.name}
+      data={friends}
+      renderItem={({item}) => {
+        return <Text style={styles.itemText}>{item.name}</Text>;
+      }}
+    />
   );
 };
 
 export default ListScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  itemText: {
+    fontSize: 30,
+  },
+});
